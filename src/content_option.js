e69f84d8412556a3b4d2a2c7bc85f25736243c98
msafeerhussain <msafeerhussain13@gmail.com>
2023-05-28 12:34:51 -0700

    first commit

---
+++ src/content_option.js
@@ -0,0 +1,136 @@
+const logotext = "Muhammad Safeer Hussain";
+const meta = {
+    title: "Muhammad Safeer Hussain",
+    description: "I’m Muhammad Safeer Hussain _ Full stack Web Devloper",
+};
+
+const introdata = {
+    title: "I’m M.Safeer Hussain",
+    animated: {
+        first: "I’m a JavaScript Enthusiast",
+        second: "I’m Currently learning react native for building mobile Application.",
+        third: "I’m expert in React.JS, Python Django & Wordpress.",
+    },
+    description: "I have more than three year of experience in  Web Designing & Development Field.",
+    your_img_url: "https://images.unsplash.com/photo-1685292278563-58a5a542310a?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwcm9maWxlLXBhZ2V8MXx8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&w=500&q=60",
+};
+const dataabout = {
+    title: "About MySelf",
+    aboutme: "Hello! I'm Muhammad Safeer Hussain, a passionate web developer dedicated to crafting exceptional digital experiences. With a strong foundation in HTML, CSS, JavaScript & Python. I bring websites to life with sleek designs and seamless functionality. I thrive on translating clients' visions into pixel-perfect reality, ensuring user-friendly interfaces and optimal performance across devices. My expertise extends to responsive design, front-end frameworks, and Backend Development. Equipped with a creative mindset and an eye for detail, I'm constantly exploring the latest industry trends and technologies to deliver innovative solutions.",
+};
+const worktimeline = [{
+        jobtitle: "Speacker In Seminar 'Tech Workshop 2023'",
+        where: "IUB",
+        date: "2023",
+    },
+    {
+        jobtitle: "Front End Bootcamp Competition Winner",
+        where: "Google DSC-IUB",
+        date: "2022",
+    },
+];
+
+const skills = [{
+        name: "JavaScript",
+        value: 90,
+    },
+    {
+        name: "Python",
+        value: 82,
+    },
+    {
+        name: "React",
+        value: 75,
+    },
+    {
+        name: "Next",
+        value: 80,
+    },
+    {
+        name: "Django",
+        value: 70,
+    },
+    {
+        name: "Graphic Designing",
+        value: 82,
+    },
+];
+
+const services = [{
+        title: "Front End Development",
+        description: "I develop Front End Template of websites in React or with the combination of HTML5, CSS3 & JS.",
+    },
+    {
+        title: "Web Development",
+        description: "I develop a website by using React or HTML5, CSS3 & JS for front end development and Python Django or Node JS for backend. Either, i develop in Next JS too.",
+    },
+    {
+        title: "Wordpress Development",
+        description: "I Design and optimize the wordpress website using Elementor, Divi.",
+    },
+    {
+        title: "Graphic Designing",
+        description: "I also Design a Catalog & Booklets.",
+    },
+];
+
+const dataportfolio = [{
+        img: "https://plus.unsplash.com/premium_photo-1676618539987-12b7c8a8ae61?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8N3x8Y29kZXxlbnwwfHwwfHx8MA%3D%3D&auto=format&fit=crop&w=500&q=60",
+        description: "Project 1",
+        link: "#",
+    },
+    {
+        img: "https://plus.unsplash.com/premium_photo-1676618539987-12b7c8a8ae61?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8N3x8Y29kZXxlbnwwfHwwfHx8MA%3D%3D&auto=format&fit=crop&w=500&q=60",
+        description: "Project 2",
+        link: "#",
+    },
+    {
+        img: "https://plus.unsplash.com/premium_photo-1676618539987-12b7c8a8ae61?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8N3x8Y29kZXxlbnwwfHwwfHx8MA%3D%3D&auto=format&fit=crop&w=500&q=60",
+        description: "Project 3",
+        link: "#",
+    },
+    {
+        img: "https://plus.unsplash.com/premium_photo-1676618539987-12b7c8a8ae61?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8N3x8Y29kZXxlbnwwfHwwfHx8MA%3D%3D&auto=format&fit=crop&w=500&q=60",
+        description: "Project 4",
+        link: "#",
+    },
+    {
+        img: "https://plus.unsplash.com/premium_photo-1676618539987-12b7c8a8ae61?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8N3x8Y29kZXxlbnwwfHwwfHx8MA%3D%3D&auto=format&fit=crop&w=500&q=60",
+        description: "Project 5",
+        link: "#",
+    },
+    {
+        img: "https://plus.unsplash.com/premium_photo-1676618539987-12b7c8a8ae61?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8N3x8Y29kZXxlbnwwfHwwfHx8MA%3D%3D&auto=format&fit=crop&w=500&q=60",
+        description: "Project 6",
+        link: "#",
+    },
+];
+
+const contactConfig = {
+    YOUR_EMAIL: "msafeerhussain@gmail.com",
+    YOUR_FONE: "+92-3351806883",
+    description: "Please Contact me, in case of any query. I will resolve it for you. ",
+    
+    YOUR_SERVICE_ID: "service_d1y96cj",
+    YOUR_TEMPLATE_ID: "template_6vtthzg",
+    YOUR_USER_ID: "JREB_xtZ3N5GJhPC-",
+};
+
+const socialprofils = {
+    github: "https://github.com/msafeerhussain",
+    facebook: "https://www.facebook.com/msafeerhussain1214/",
+    linkedin: "https://www.linkedin.com/in/muhammad-safeer-hussain-845538205/",
+    twitter: "https://twitter.com/Muhamma66917301",
+};
+export {
+    meta,
+    dataabout,
+    dataportfolio,
+    worktimeline,
+    skills,
+    services,
+    introdata,
+    contactConfig,
+    socialprofils,
+    logotext,
+};
